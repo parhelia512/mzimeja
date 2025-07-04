dfdda3e3b3d00964716422c9e784d50173c37698
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-07-04 09:26:05 +0900

    InvalidateRectWrap

--- ime/mzimeja.h
+++ ime/mzimeja.h
@@ -110,6 +110,17 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
+static inline BOOL
+InvalidateRectWrap(HWND hwnd, LPCRECT prc, BOOL bErase, const char *file, INT lineno)
+{
+    if (!hwnd)
+        DPRINTA("InvalidateRect(NULL) detected at File %s, Line %d\n", file, lineno);
+    return InvalidateRect(hwnd, prc, bErase);
+}
+#define InvalidateRect(hwnd, prc, bErase) InvalidateRectWrap(hwnd, prc, bErase, __FILE__, __LINE__)
+
+//////////////////////////////////////////////////////////////////////////////
+
 // For limit of MZ-IME.
 // MZ-IMEの制限。
 #define MAXCOMPWND  10  // maximum number of composition windows
